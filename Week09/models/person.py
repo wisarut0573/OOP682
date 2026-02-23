5b5a6027211acb4e0c499d9259549cf941fe1789
wisarut <wisarut.bu.68@ubu.ac.th>
2026-02-23 23:55:58 +0700

    Cleanup unwanted folders

--- Week09/models/person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, pid, name, age):
-        self.pid = pid
-        self.name = name
-        self.age = age
-
-    def __str__(self):
-        return f"Person[{self.pid}, {self.name}, {self.age}]"
